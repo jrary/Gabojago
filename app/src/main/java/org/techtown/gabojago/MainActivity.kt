16978dfc714ee0602babd65bbb609c171b260731
jrary <73732743+jrary@users.noreply.github.com>
2022-01-10 17:45:26 +0900

    menu

--- app/src/main/java/org/techtown/gabojago/MainActivity.kt
+++ app/src/main/java/org/techtown/gabojago/MainActivity.kt
@@ -2,10 +2,54 @@ package org.techtown.gabojago
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.techtown.gabojago.databinding.ActivityMainBinding
+import org.techtown.gabojago.randomPick.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun initNavigation() {
+        supportFragmentManager.beginTransaction().replace(R.id.main_frm, HomeFragment())
+            .commitAllowingStateLoss()
+
+        binding.mainBnv.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.homeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.lookFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, LookFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.searchFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, SearchFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.lockerFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, LockerFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+            }
+            false
+        }
     }
 }
---
+++ app/src/main/java/org/techtown/gabojago/randomPick/HomeFragment.kt
@@ -0,0 +1,6 @@
+package org.techtown.gabojago.randomPick
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+}
