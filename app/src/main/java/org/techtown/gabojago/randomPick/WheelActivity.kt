9ebe65e63e1383d0af67cbf6e9d907cf393642f9
jrary <73732743+jrary@users.noreply.github.com>
2022-01-10 19:50:15 +0900

    Connect WheelActivity

--- app/src/main/java/org/techtown/gabojago/randomPick/WheelActivity.kt
+++ app/src/main/java/org/techtown/gabojago/randomPick/WheelActivity.kt
@@ -6,8 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import org.techtown.gabojago.databinding.ActivityWheelBinding
+import org.techtown.gabojago.databinding.FragmentHomeBinding
 
 class WheelActivity : AppCompatActivity() {
     lateinit var binding: ActivityWheelBinding
 
+    override fun onCreate(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = ActivityWheelBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
 }
