dd2a670676eb88450599ff09ed0489ae00a54588
jrary <73732743+jrary@users.noreply.github.com>
2022-01-10 13:36:53 +0900

    Github test - SplashActivity

--- app/src/main/java/org/techtown/gabojago/SplashActivity.kt
+++ app/src/main/java/org/techtown/gabojago/SplashActivity.kt
@@ -1,13 +1,10 @@
 package org.techtown.gabojago
 
 import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.View
-import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
