64a7feb6c65b44653b75a2f73ad186a8ee609b4c
jrary <73732743+jrary@users.noreply.github.com>
2022-01-10 18:36:11 +0900

    Ex

--- app/src/main/java/org/techtown/gabojago/randomPick/WheelActivity.kt
+++ app/src/main/java/org/techtown/gabojago/randomPick/WheelActivity.kt
@@ -1,5 +1,18 @@
 package org.techtown.gabojago.randomPick
 
-class WheelActivity {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import org.techtown.gabojago.databinding.ActivityWheelBinding
 
+class WheelActivity : AppCompatActivity() {
+    lateinit var binding: ActivityWheelBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = ActivityWheelBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
 }
