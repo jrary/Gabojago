3f512a4b5b387135b4bdbc1f1cd00d21ff930d3e
CHAEYOUNG SONG <thdcodud103@naver.com>
2022-01-15 16:52:43 +0900

    캘린더 리사이클러뷰 연결

--- app/src/main/java/org/techtown/gabojago/RecordFragment.kt
+++ app/src/main/java/org/techtown/gabojago/RecordFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import org.techtown.gabojago.databinding.FragmentRecordBinding
 
 class RecordFragment : Fragment() {
@@ -18,6 +19,12 @@ class RecordFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentRecordBinding.inflate(inflater, container, false)
+
+        binding.recordWeekRecyclerview.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+        val recordWeekRVAdapter = RecordWeekRVAdapter()
+        binding.recordWeekRecyclerview.adapter = recordWeekRVAdapter
+
 
         binding.recordMonthTv.setOnClickListener{
             startActivity(Intent(activity, CalendarActivity::class.java))
