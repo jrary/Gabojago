f1c817a7e2dfc95ea2e4f4b86a56714d8e0cf6b5
jrary <73732743+jrary@users.noreply.github.com>
2022-01-23 06:15:07 +0900

    Login API - not yet
    With naver developers
    Not enough information with Kotlin...
    Finding some github results

--- app/src/main/java/org/techtown/gabojago/LoginActivity.kt
+++ app/src/main/java/org/techtown/gabojago/LoginActivity.kt
@@ -6,9 +6,12 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.nhn.android.naverlogin.OAuthLogin
+import com.nhn.android.naverlogin.OAuthLogin.mOAuthLoginHandler
 import com.nhn.android.naverlogin.OAuthLoginHandler
+import kotlinx.android.synthetic.main.activity_login.*
 import org.techtown.gabojago.databinding.ActivityLoginBinding
 import org.techtown.gabojago.databinding.ActivityWheelSelectBinding
 
@@ -31,11 +34,40 @@ class LoginActivity :AppCompatActivity() {
 
         mContext = this
 
-      // binding.buttonOathLogin.setO
+        mOAuthLoginInstance = OAuthLogin.getInstance()
+        mOAuthLoginInstance.init(mContext, clientId, clientSecret, clientName, clientEmail)
+        binding.loginNaverBtn.setOAuthLoginHandler(mOAuthLoginHandler)
 
         binding.loginCompBtn.setOnClickListener {
             startActivity(Intent(this@LoginActivity, MainActivity::class.java))
             finish()
+        }
+    }
+
+    private val mOAuthLoginHandler: OAuthLoginHandler = object: OAuthLoginHandler(){
+        override fun run(success: Boolean) {
+            if (success) {
+                Toast.makeText(baseContext,"Login Success", Toast.LENGTH_SHORT).show()
+                val accessToken: String = mOAuthLoginInstance.getAccessToken(baseContext)
+                val refreshToken: String = mOAuthLoginInstance.getRefreshToken(baseContext)
+                val expiresAt: Long = mOAuthLoginInstance.getExpiresAt(baseContext)
+                val tokenType: String = mOAuthLoginInstance.getTokenType(baseContext)
+                Toast.makeText(
+                    baseContext, "Success:" + accessToken + " : "
+                            + refreshToken + " : " + expiresAt + " : "
+                            + tokenType, Toast.LENGTH_SHORT
+                ).show()
+                var intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                val errorCode: String = mOAuthLoginInstance.getLastErrorCode(mContext).code
+                val errorDesc = mOAuthLoginInstance.getLastErrorDesc(mContext)
+
+                Toast.makeText(
+                    baseContext, "errorCode:" + errorCode
+                            + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 }
