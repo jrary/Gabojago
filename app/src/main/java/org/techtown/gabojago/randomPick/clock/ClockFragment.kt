bb1ee6d99379ae0f2f23e8c7d377ff1b1e8792ae
jrary <73732743+jrary@users.noreply.github.com>
2022-01-28 20:46:12 +0900

    Update ClockFragment.kt
    Function and animation complete

--- app/src/main/java/org/techtown/gabojago/randomPick/clock/ClockFragment.kt
+++ app/src/main/java/org/techtown/gabojago/randomPick/clock/ClockFragment.kt
@@ -3,8 +3,12 @@ package org.techtown.gabojago.randomPick.clock
 import android.animation.Animator
 import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -44,6 +48,21 @@ class ClockFragment : Fragment() {
             360f
     )
 
+    var viewArr = arrayOf(
+        binding.clock1,
+        binding.clock2,
+        binding.clock3,
+        binding.clock4,
+        binding.clock5,
+        binding.clock6,
+        binding.clock7,
+        binding.clock8,
+        binding.clock9,
+        binding.clock10,
+        binding.clock11,
+        binding.clock12
+    )
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -81,19 +100,24 @@ class ClockFragment : Fragment() {
                 binding.clockRetryBtn.visibility = View.VISIBLE
                 binding.clockSaveBtn.visibility = View.VISIBLE
                 binding.clockResultTv.visibility = View.VISIBLE
-            }, 1500)
+            }, 4700)
         }
         binding.clockRetryBtn.setOnClickListener {
-            binding.clockOptionBtn.visibility = View.VISIBLE
-            binding.clockGoBtn.visibility = View.VISIBLE
-            binding.clockInfoTitleTv.visibility = View.VISIBLE
-            binding.clockInfoTv.visibility = View.VISIBLE
-            binding.clockRetryBtn.visibility = View.GONE
-            binding.clockSaveBtn.visibility = View.GONE
-            binding.clockResultTv.visibility = View.GONE
             val rotateAnimationResult = RotateAnimation(0F, 0F, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
             rotateAnimationResult.fillAfter = true
             binding.clockArrowIv.startAnimation(rotateAnimationResult)
+            binding.clockResultTv.visibility = View.GONE
+            binding.clockRetryBtn.visibility = View.GONE
+            binding.clockSaveBtn.visibility = View.GONE
+            Handler().postDelayed({
+                moveClock()
+            }, 200)
+            Handler().postDelayed({
+                binding.clockResultTv.text = getResClock.toString() + "시 방향"
+                binding.clockRetryBtn.visibility = View.VISIBLE
+                binding.clockSaveBtn.visibility = View.VISIBLE
+                binding.clockResultTv.visibility = View.VISIBLE
+            }, 4900)
         }
         binding.clockSaveBtn.setOnClickListener{
             Toast.makeText(
@@ -119,53 +143,105 @@ class ClockFragment : Fragment() {
         binding.clockInfoTv.visibility = View.VISIBLE
     }
 
+    //startNum, endNum의 범위는 1~12이다.
+    //index의 범위는 0~11이다
+    //getResClock으로 넘겨주는 값은 0~11이고, getResClock으로 리턴받는 값은 1~12이다.
     private fun moveClock(){
         var startAngle: Float
         var endAngle: Float
         var resAngle: Float
         if(startNum > endNum){
-            getResClock = getClockResult(startNum, endNum + 12)
+            getResClock = getClockResult(startNum - 1, endNum + 12 - 1)
             startAngle = -360f + clockAngle[startNum - 1]
             endAngle = clockAngle[endNum - 1]
             if (getResClock <= 12){
                 resAngle = -360f + clockAngle[getResClock - 1]
             }
             else{
+                getResClock %= 12
                 resAngle = clockAngle[getResClock - 1]
             }
         }
         else{
-            getResClock = getClockResult(startNum + 12, endNum)
-            startAngle = clockAngle[getResClock - 1]
-            endAngle = clockAngle[getResClock - 1]
+            getResClock = getClockResult(startNum - 1, endNum - 1)
+            startAngle = clockAngle[startNum - 1]
+            endAngle = clockAngle[endNum - 1]
             resAngle = clockAngle[getResClock - 1]
         }
+        clockRangeSetter(startAngle, endAngle)
+        clockAnimation(startAngle, endAngle, resAngle)
+    }
+    private fun getClockResult(start: Int, end: Int): Int{
+        val random = Random()
+        Log.d("getStartandEndClock", startNum.toString()+" / "+endNum.toString())
+        Log.d("clockBound01", start.toString()+" / "+end.toString())
+        val bound = end - start + 1
+        Log.d("clockBound02", bound.toString())
+        val res = start + random.nextInt(bound)
+        Log.d("clockResultRandomFunc", res.toString())
+        return res + 1
+    }
+
+    private fun clockAnimation(startAngle: Float, endAngle: Float, resAngle: Float){
         Handler().postDelayed({
             val rotateAnimation = RotateAnimation(0f, startAngle, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
-            rotateAnimation.duration = 300
+            rotateAnimation.duration = 100
             rotateAnimation.fillAfter = true
             //   rotateAnimation.interpolator =
             binding.clockArrowIv.startAnimation(rotateAnimation)
         }, 100)
         Handler().postDelayed({
             val rotateAnimation = RotateAnimation(startAngle, endAngle, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
-            rotateAnimation.duration = 400
+            rotateAnimation.duration = 300
             rotateAnimation.fillAfter = true
             //   rotateAnimation.interpolator =
             binding.clockArrowIv.startAnimation(rotateAnimation)
-        }, 600)
+        }, 400)
         Handler().postDelayed({
-            val rotateAnimationResult = RotateAnimation(endAngle, resAngle, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+            val rotateAnimation = RotateAnimation(endAngle, startAngle, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+            rotateAnimation.duration = 300
+            rotateAnimation.fillAfter = true
+            //   rotateAnimation.interpolator =
+            binding.clockArrowIv.startAnimation(rotateAnimation)
+        }, 700)
+        Handler().postDelayed({
+            val rotateAnimation = RotateAnimation(startAngle, endAngle, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+            rotateAnimation.duration = 500
+            rotateAnimation.fillAfter = true
+            //   rotateAnimation.interpolator =
+            binding.clockArrowIv.startAnimation(rotateAnimation)
+        }, 1000)
+        Handler().postDelayed({
+            val rotateAnimation = RotateAnimation(endAngle, startAngle, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+            rotateAnimation.duration = 500
+            rotateAnimation.fillAfter = true
+            //   rotateAnimation.interpolator =
+            binding.clockArrowIv.startAnimation(rotateAnimation)
+        }, 1500)
+        Handler().postDelayed({
+            val rotateAnimation = RotateAnimation(startAngle, endAngle, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+            rotateAnimation.duration = 800
+            rotateAnimation.fillAfter = true
+            //   rotateAnimation.interpolator =
+            binding.clockArrowIv.startAnimation(rotateAnimation)
+        }, 2000)
+        Handler().postDelayed({
+            val rotateAnimation = RotateAnimation(endAngle, startAngle, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+            rotateAnimation.duration = 800
+            rotateAnimation.fillAfter = true
+            //   rotateAnimation.interpolator =
+            binding.clockArrowIv.startAnimation(rotateAnimation)
+        }, 2800)
+        Handler().postDelayed({
+            val rotateAnimationResult = RotateAnimation(startAngle, resAngle, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
             rotateAnimationResult.fillAfter = true
-            rotateAnimationResult.duration = 300
+            rotateAnimationResult.duration = 900
             //   rotateAnimation.interpolator =
             binding.clockArrowIv.startAnimation(rotateAnimationResult)
-        }, 1000)
+        }, 3600)
     }
-    private fun getClockResult(start: Int, end: Int): Int{
-        val random = Random()
-        val res = (start + random.nextInt(end - start)) % 12
-        Log.d("clockResultRandomFunc", res.toString())
-        return res
+    private fun clockRangeSetter(startAngle: Float, endAngle: Float){
+        binding.clock1.setTextColor(Color.rgb(255,103,69))
+        binding.clock2.setTextColor(Color.rgb(255,147,124))
     }
 }
