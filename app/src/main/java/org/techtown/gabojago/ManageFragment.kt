45735f65a052624f3907394be416f4f718ccfcab
jrary <73732743+jrary@users.noreply.github.com>
2022-02-01 15:24:53 +0900

    Create Manage Fragment

--- app/src/main/java/org/techtown/gabojago/ManageFragment.kt
+++ app/src/main/java/org/techtown/gabojago/ManageFragment.kt
@@ -1,6 +1,24 @@
 package org.techtown.gabojago
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import org.techtown.gabojago.databinding.FragmentManageBinding
+import org.techtown.gabojago.databinding.FragmentWheelBinding
 
 class ManageFragment : Fragment() {
+    lateinit var binding: FragmentManageBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentManageBinding.inflate(layoutInflater)
+        
+
+        return binding.root
+    }
 }
