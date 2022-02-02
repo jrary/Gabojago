9cbccbc76d1a339306a11bd69b29d8891c7f7cd5
CHAEYOUNG SONG <thdcodud103@naver.com>
2022-02-02 18:43:55 +0900

    기록하자 세부수정 및 기능추가

--- app/src/main/java/org/techtown/gabojago/Record/RecordFolderResultRVAdapter.kt
+++ app/src/main/java/org/techtown/gabojago/Record/RecordFolderResultRVAdapter.kt
@@ -3,11 +3,9 @@ package org.techtown.gabojago.Record
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import org.techtown.gabojago.databinding.ItemRecordResultBinding
-import org.techtown.gabojago.databinding.ItemRecordWeekBinding
-import java.util.ArrayList
+import org.techtown.gabojago.databinding.ItemRecordFolderBinding
 
-class RecordResultRVAdapter: RecyclerView.Adapter<RecordResultRVAdapter.ViewHolder>() {
+class RecordFolderResultRVAdapter: RecyclerView.Adapter<RecordFolderResultRVAdapter.ViewHolder>() {
 
     //클릭 인터페이스
     interface MyItemClickListener {
@@ -22,8 +20,8 @@ class RecordResultRVAdapter: RecyclerView.Adapter<RecordResultRVAdapter.ViewHold
 
     //뷰홀더 생성->호출되는 함수->아이템 뷰 객체를 만들어서 뷰홀더에 던져줌
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding: ItemRecordResultBinding =
-            ItemRecordResultBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding: ItemRecordFolderBinding =
+            ItemRecordFolderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
         return ViewHolder(binding)
     }
@@ -31,13 +29,13 @@ class RecordResultRVAdapter: RecyclerView.Adapter<RecordResultRVAdapter.ViewHold
     //뷰홀더에 데이터를 바인딩해줘야 할 때마다 호출되는 함수 => 엄청나게 많이 호출
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind()
-        holder.binding.itemRecordPecilIv.setOnClickListener {
+        holder.itemView.setOnClickListener {
             mItemClickListener.onItemClick()
         }
     }
 
     //뷰홀더
-    inner class ViewHolder(val binding: ItemRecordResultBinding) :
+    inner class ViewHolder(val binding: ItemRecordFolderBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind() {
         }
@@ -45,7 +43,7 @@ class RecordResultRVAdapter: RecyclerView.Adapter<RecordResultRVAdapter.ViewHold
     }
 
     override fun getItemCount(): Int {
-        return 4
+        return 3
     }
 
 }
