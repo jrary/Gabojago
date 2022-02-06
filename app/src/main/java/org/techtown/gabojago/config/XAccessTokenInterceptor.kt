669028826d67f5a296ef4d5cdd8c9fd6b1477eb8
jrary <73732743+jrary@users.noreply.github.com>
2022-02-06 19:16:45 +0900

    Catch Error

--- app/src/main/java/org/techtown/gabojago/config/XAccessTokenInterceptor.kt
+++ app/src/main/java/org/techtown/gabojago/config/XAccessTokenInterceptor.kt
@@ -1,21 +1,21 @@
-package org.techtown.gabojago.config
-
-import okhttp3.Interceptor
-import okhttp3.Request
-import okhttp3.Response
-import org.techtown.gabojago.ApplicationClass.Companion.X_ACCESS_TOKEN
-import org.techtown.gabojago.main.getJwt
-
-class XAccessTokenInterceptor: Interceptor {
-    override fun intercept(chain: Interceptor.Chain): Response {
-        val builder: Request.Builder = chain.request().newBuilder()
-
-        val jwtToken: String? = getJwt()
-
-        jwtToken?.let{
-            builder.addHeader(X_ACCESS_TOKEN, jwtToken)
-        }
-
-        return chain.proceed(builder.build())
-    }
-}
+//package org.techtown.gabojago.config
+//
+//import okhttp3.Interceptor
+//import okhttp3.Request
+//import okhttp3.Response
+//import org.techtown.gabojago.ApplicationClass.Companion.X_ACCESS_TOKEN
+//import org.techtown.gabojago.main.getJwt
+//
+//class XAccessTokenInterceptor: Interceptor {
+//    override fun intercept(chain: Interceptor.Chain): Response {
+//        val builder: Request.Builder = chain.request().newBuilder()
+//
+//        val jwtToken: String? = getJwt()
+//
+//        jwtToken?.let{
+//            builder.addHeader(X_ACCESS_TOKEN, jwtToken)
+//        }
+//
+//        return chain.proceed(builder.build())
+//    }
+//}
