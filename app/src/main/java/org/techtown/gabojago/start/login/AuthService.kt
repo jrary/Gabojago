fc27e1cb22e669fab2dc6773482ad1b9424886cc
jrary <73732743+jrary@users.noreply.github.com>
2022-02-26 20:37:07 +0900

    Auto Login

--- app/src/main/java/org/techtown/gabojago/start/login/AuthService.kt
+++ app/src/main/java/org/techtown/gabojago/start/login/AuthService.kt
@@ -9,9 +9,14 @@ import retrofit2.Response
 class AuthService {
 
     private lateinit var loginView: LoginView
+    private lateinit var remainLoginView: RemainLoginView
 
     fun setLoginView(loginView: LoginView){
         this.loginView = loginView
+    }
+
+    fun setRemainLoginView(remainLoginView: RemainLoginView){
+        this.remainLoginView = remainLoginView
     }
 
     fun login(loginToken: String) {
@@ -31,6 +36,27 @@ class AuthService {
             }
             override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
                 loginView.onLoginFailure(400, t.message!!)
+            }
+        })
+    }
+
+    fun remainLogin(loginToken: String) {
+        val authService = getRetrofit().create(AuthRetrofitInterface::class.java)
+        authService.remainLogin(loginToken).enqueue(object : Callback<AuthRemainResponse> {
+            override fun onResponse(call: Call<AuthRemainResponse>, response: Response<AuthRemainResponse>) {
+                Log.d("RELOGINACT/Response", response.toString())
+                val resp = response.body()!!
+                Log.d("RELOGINACT/Code", resp.code.toString())
+
+                if(resp.isSuccess){
+                    remainLoginView.onRemainLoginSuccess(resp.result)
+                }
+                else{
+                    remainLoginView.onRemainLoginFailure(resp.code, resp.message)
+                }
+            }
+            override fun onFailure(call: Call<AuthRemainResponse>, t: Throwable) {
+                remainLoginView.onRemainLoginFailure(400, t.message!!)
             }
         })
     }
